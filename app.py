48f60b9af217d3457a407ef472148b54804c713c
Alsyahrir <153171771+Alsyahrir@users.noreply.github.com>
2023-12-13 21:23:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,8 +40,36 @@ def diagnosis(file, model, IMM_SIZE):
 
 # ...
 
+
+def set_background(image_file):
+    """
+    This function sets the background of a Streamlit app to an image specified by the given image file.
+
+    Parameters:
+        image_file (str): The path to the image file to be used as the background.
+
+    Returns:
+        None
+    """
+    with open(image_file, "rb") as f:
+        img_data = f.read()
+    b64_encoded = base64.b64encode(img_data).decode()
+    style = f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/png;base64,{b64_encoded});
+            background-size: cover;
+        }}
+        </style>
+    """
+    st.markdown(style, unsafe_allow_html=True)
+    
 # Main Streamlit app
 def main():
+    
+    set_background('bg5.png')
+    
+    
     st.title("Chest X-ray Image Diagnosis App")
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
